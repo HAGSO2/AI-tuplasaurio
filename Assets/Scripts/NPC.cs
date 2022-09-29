051afc116abb380a45de89c42843aeed33b1876b
HAGSO2 <83025616+HAGSO2@users.noreply.github.com>
2022-09-29 13:38:13 +0200

    speed and orientation

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -12,22 +12,35 @@ public class NPC : MonoBehaviour
     private List<NPC> _nearNpCs;
     private float _heatCrumbs; // At the time og making a heat map...
     //private float _maxDist; // Will be used in skeletons
-    private float _vel; // Velocity max magnitude
+    private float _vel = 5.5f; // Velocity max magnitude
     public bool Alarmed { get; private set; }
-    public NPC(bool alarmed)
+
+    private void set_speed(Vector3 runnigAt)
     {
-        Alarmed = alarmed;
-        _body = gameObject.GetComponent<Rigidbody>();
+        //Changes speed direction to go to the runningPoint
+
+        Vector3 desired = runnigAt - transform.position;
+        Vector3 steering = desired - _body.velocity;
+        steering.Normalize();
+        _body.AddForce(steering * _vel);
+    }
+
+    private void set_Orientation(Vector3 looking, float time)
+    {
+        Vector3 desired = looking - transform.position;
+        transform.forward = Vector3.Lerp(transform.forward, desired.normalized, time);
     }
     // Start is called before the first frame update
     void Start()
     {
+        _body = gameObject.GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        set_Orientation(player.position,Time.deltaTime);
+        set_speed(player.position);
     }
 }
