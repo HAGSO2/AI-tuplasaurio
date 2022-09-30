41c3e348be8b297df5430b2ad7a814ddcba1adee
HAGSO2 <83025616+HAGSO2@users.noreply.github.com>
2022-09-30 08:23:54 +0200

    set speed finished

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -12,7 +12,8 @@ public class NPC : MonoBehaviour
     private List<NPC> _nearNpCs;
     private float _heatCrumbs; // At the time og making a heat map...
     //private float _maxDist; // Will be used in skeletons
-    private float _vel = 5.5f; // Velocity max magnitude
+    private float _vel = 1.5f; // Velocity max magnitude
+    private Vector3 _movDir;
     public bool Alarmed { get; private set; }
 
     private void set_speed(Vector3 runnigAt)
@@ -21,8 +22,18 @@ public class NPC : MonoBehaviour
 
         Vector3 desired = runnigAt - transform.position;
         Vector3 steering = desired - _body.velocity;
+        Debug.DrawLine(transform.position,player.position,Color.black);
+        Debug.DrawRay(transform.position,steering.normalized * _vel);
         steering.Normalize();
-        _body.AddForce(steering * _vel);
+        if(Vector3.Angle(desired,steering) < 30 || (transform.position - runnigAt).magnitude > 10)
+        {
+            _movDir = steering;
+            _body.AddForce(steering * _vel);
+        }
+        else
+        {
+            _body.AddForce(_movDir * _vel);
+        }
     }
 
     private void set_Orientation(Vector3 looking, float time)
